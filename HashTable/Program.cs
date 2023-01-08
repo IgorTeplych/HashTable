19747c7a2e6095f95280fd1d512495970dad4a67
Igor Teplych <igorvrt@mail.ru>
2023-01-08 21:45:46 +0600

    Добавил реализацию хэш таблицы

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,7 +1,23 @@
-﻿static class Programm
+﻿using HashTable;
+
+static class Programm
 {
+    static Random rnd = new Random();
     public static void Main()
     {
+        Table table = new Table();
+        table.Add(12);
+        table.Add(13);
+        table.Add(15);
+        table.Add(19);
+        table.Add(18);
+        table.Add(58);
+        table.Add(258);
+        table.Add(5228);
+        table.Add(5518);
+        table.Add(5815615);
+        table.Add(581156);
 
+        var vr = table.Search(25858);
     }
 }
---
+++ HashTable/Table.cs
@@ -0,0 +1,76 @@
+﻿using BasicStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    internal class Table
+    {
+        FactorArray<Node> hashes;
+        public Table()
+        {
+            hashes = new FactorArray<Node>();
+
+        }
+
+        public void Delete()
+        {
+
+        }
+        public Node Search(int key)
+        {
+            int hash = GetHash(key);
+            int count = 0; 
+            Node H;
+            while (count < hashes.Size && hashes.Get(count).Key != hash)
+            {
+                count++;
+            }
+
+            if(count >= hashes.Size)
+                return null;
+
+            H = hashes.Get(count);
+
+            Node c = H.Child;
+            while (c.Key != key)
+            {
+                c = c.Child;
+            }
+            return c;
+        }
+        public void Add(int key)
+        {
+            Add(new Node(key));
+        }
+        void Add(Node node)
+        {
+            Node hash = new Node(GetHash(node.Key));
+            AddInTable(hash, node);
+        }
+        void AddInTable(Node hash, Node node)
+        {
+            Node searh = hashes.Search(hash, out bool result);
+            if (result)
+            {
+                Node temp = searh;
+                while(temp.Child != null)
+                    temp = temp.Child;
+                temp.Child = node;
+            }
+            else
+            {
+                searh.Child = node;
+                hashes.Add(searh);
+            }
+        }
+
+        int GetHash(int key)
+        {
+            return key % ((int)Math.Log10(key) + 1);
+        }
+    }
+}
