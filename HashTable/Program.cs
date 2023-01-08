905647d491ef2eb19b713ac753f64170c4841219
Igor Teplych <igorvrt@mail.ru>
2023-01-08 22:43:09 +0600

    Добавил тесты производительности с выводом в консоль.

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,27 +1,87 @@
 ﻿using HashTable;
+using System.Diagnostics;
 
 static class Programm
 {
     static Random rnd = new Random();
     public static void Main()
     {
+        RandomNumbersPerformTest();
+        Console.WriteLine();
+        OrderedNumbersPerformTest();
+        Console.ReadLine();
+    }
+    static void RandomNumbersPerformTest()
+    {
+        int N = 10000;
+        int[] rndNum = GetRandomNumbers(N);
+
         Table table = new Table();
-        table.Add(12);
-        table.Add(13);
-        table.Add(15);
-        table.Add(19);
-        table.Add(18);
-        table.Add(58);
-        table.Add(258);
-        table.Add(5228);
-        table.Add(5518);
-        table.Add(5815615);
-        table.Add(581156);
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < N; i++)
+        {
+            table.Add(rndNum[i]);
+        }
+        sw.Stop();
+        Console.WriteLine($"Добавление в [HashTable] неупорядоченных чисел от 1 до {N} занимает: {sw.ElapsedTicks * 100} нс.");
 
-        var vr = table.Search(258);
+        sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < (N / 10); i++)
+        {
+            Node searh = table.Search(rndNum[random.Next(0, rndNum.Length - 1)]);
+        }
+        sw.Stop();
+        Console.WriteLine($"Поиск в [HashTable] {N / 10} случайных чисел занимает: {sw.ElapsedTicks * 100} нс.");
+        Console.WriteLine();
+    }
+    static void OrderedNumbersPerformTest()
+    {
+        int N = 10000;
+        int[] orderNum = GetOrderNumbers(N);
 
-        table.Delete(258);
+        Table table = new Table();
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < N; i++)
+        {
+            table.Add(orderNum[i]);
+        }
+        sw.Stop();
+        Console.WriteLine($"Добавление в [HashTable] упорядоченных чисел от 1 до {N} занимает: {sw.ElapsedTicks * 100} нс.");
 
-        vr = table.Search(258);
+        sw = new Stopwatch();
+        sw.Start();
+        for (int i = 0; i < (N / 10); i++)
+        {
+            Node searh = table.Search(orderNum[random.Next(0, orderNum.Length - 1)]);
+        }
+        sw.Stop();
+        Console.WriteLine($"Поиск в [HashTable] {N / 10} случайных чисел занимает: {sw.ElapsedTicks * 100} нс.");
+        Console.WriteLine();
+    }
+
+    static Random random = new Random();
+    static int[] GetRandomNumbers(int N)
+    {
+        int[] mass = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            if (i != N)
+                mass[i] = random.Next(1, int.MaxValue);
+        }
+        return mass;
+    }
+
+    static int[] GetOrderNumbers(int N)
+    {
+        int[] mass = new int[N];
+        for (int i = 1; i < N; i++)
+        {
+            if (i != N)
+                mass[i] = i;
+        }
+        return mass;
     }
 }
